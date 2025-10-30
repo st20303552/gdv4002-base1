a164aa77533b1f25f580470a32dbb9fc8bdce91b
st20303552 <st20303552@S01111>
2025-10-30 17:47:23 +0000

    Maths Workshop 5

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -1,6 +1,7 @@
 #include "Engine.h"
 
 // Function prototypes
+void myUpdate(GLFWwindow* window, double tDelta);
 
 
 int main(void) {
@@ -17,17 +18,53 @@ int main(void) {
 
 	//
 	// Setup game scene objects here
+	// addObject("player", glm::vec2(1.0f, 1.0f), glm::radians (45.0f), glm::vec2(0.5f, 1.0f), "Resources\\Textures\\Bite.png");
+
+	addObject("player1");
+	addObject("player2");
+	GameObject2D* player1Object = getObject("player1");
+
+	if (player1Object != nullptr)
+	{
+		// Update player1 here
+		player1Object->position = glm::vec2(-1.5f, 1.0f);
+		player1Object->orientation = glm::radians(-30.0f);
+		player1Object->size = glm::vec2(1.5f, 0.7f);
+		player1Object->textureID = loadTexture("Resources\\Textures\\true.png");
+	}
+
+	GameObject2D* player2Object = getObject("player2");
+
+	if (player2Object != nullptr)
+	{
+		// Update player2 here
+		player2Object->position = glm::vec2(1.5f, 1.0f);
+		player2Object->orientation = glm::radians(45.0f);
+		player2Object->size = glm::vec2(0.7f, 1.6f);
+		player2Object->textureID = loadTexture("Resources\\Textures\\bumblebee.png");
+	}
+
 	//
-	
+	setUpdateFunction(myUpdate);
 
 	// Enter main loop - this handles update and render calls
 	engineMainLoop();
+	
 
 	// When we quit (close window for example), clean up engine resources
 	engineShutdown();
 
 	// return success :)
 	return 0;
+}
+
+void myUpdate(GLFWwindow* window, double tDelta)
+{
+	setViewplaneWidth(10.0f);
+	float player1RotationSpeed = glm::radians(90.0f);
+
+	GameObject2D* player1Object = getObject("player1");
+	player1Object->orientation += player1RotationSpeed * tDelta;
 }
 
 
